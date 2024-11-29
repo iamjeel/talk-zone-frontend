13a7e4a9ae1862a31f21243e62520efacd27733d
Iamjeel <thumar.jeel@gmail.com>
2024-11-28 19:04:11 -0800

    changed home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
@@ -9,16 +9,7 @@ const Home = () => {
     <div className="Home">
       <h1>Welcome to Unsaid.</h1>
       <p>Click the button below to join a room:</p>
-      <button onClick={() => navigate('/room')}>Go to Room</button>
-
-      <div className="navigation-links">
-        <Link to="/profile">
-          <button>My Profile</button>
-        </Link>
-        <Link to="/room-history">
-          <button>Room History</button>
-        </Link>
-      </div>
+      <button onClick={() => navigate('/room')}>Join Nearby Room</button>
     </div>
   );
 };
